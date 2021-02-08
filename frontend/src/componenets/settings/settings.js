cfb8bfd3d41d88fa916359207b3df040c7301457
Kacper Kwiatkowski <kacper18k@gmail.com>
2021-02-08 17:43:32 +0100

    Security amendments.

--- frontend/src/componenets/settings/settings.js
+++ frontend/src/componenets/settings/settings.js
@@ -3,67 +3,6 @@ import React, {useState, useEffect} from "react";
 
 const Settings = () => {
 
-    const[users, setUsers] = useState([]);
-
-    useEffect(() => {
-        fetchUsers();
-    }, [])
-
-    const fetchUsers = async () => {
-        Axios.get(`http://localhost:8080/api/user/page`, {
-            params: {
-                pageNo: 0,
-                pageSize: 10,
-                sortBy: 'id',
-                sortOrder: 'ASC'
-            }})
-          .then(res => {
-            console.log(res)
-            setUsers(res.data)
-          });
-    }
-
-    const renderTable = () => {
-        return(
-            <table>
-                <thead>
-                    <tr>
-                        {renderTableHead()}
-                    </tr>
-                </thead>
-                <tbody>
-                    {renderTableBody()}
-                </tbody>
-            </table>
-        )
-    }
-
-    const renderTableHead = () => {
-            return Object.entries(users[0]).map(([key]) => {
-                return (
-                        <td>
-                            {key}
-                        </td>
-                )
-            }
-        )
-    }
-
-    const renderTableBody = () => {
-        return users.map((user) => {
-            return Object.entries(user).map(([key, value]) => {
-                return (
-                    <tr>
-                        <th>
-                            {value}
-                        </th>
-                    </tr>
-                )
-            }
-        )
-    })}
-
-    return renderTable();
 }
 
 export default Settings
