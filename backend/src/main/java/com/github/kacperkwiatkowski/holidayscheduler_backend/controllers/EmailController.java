e540f95ed2861f35686056341f8686355eb65659
Kacper Kwiatkowski <kacper18k@gmail.com>
2021-03-04 16:50:21 +0100

    Email sending functional.

--- backend/src/main/java/com/github/kacperkwiatkowski/holidayscheduler_backend/controllers/EmailController.java
+++ backend/src/main/java/com/github/kacperkwiatkowski/holidayscheduler_backend/controllers/EmailController.java
@@ -1,12 +1,12 @@
 package com.github.kacperkwiatkowski.holidayscheduler_backend.controllers;
 
-import com.github.kacperkwiatkowski.holidayscheduler_backend.model.Vacation;
-import com.github.kacperkwiatkowski.holidayscheduler_backend.repository.TeamRepository;
-import com.github.kacperkwiatkowski.holidayscheduler_backend.repository.UserRepository;
-import com.github.kacperkwiatkowski.holidayscheduler_backend.repository.VacationRepository;
+import com.github.kacperkwiatkowski.holidayscheduler_backend.config.EmailConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,24 +16,33 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/email")
 public class EmailController {
 
-    private final UserRepository userRepository;
-
-    public EmailController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     @PostMapping(path = "/send/{id}")
     @PreAuthorize("hasAuthority('self:edit')")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity createVacation(
+    public ResponseEntity sendAnEmail(
             @PathVariable("id") int id,
             @RequestParam("address") String address,
             @RequestParam("title") String title,
             @RequestParam("content") String content) {
 
         //TODO Apply logic
-        Vacation vacation = new Vacation();
-        log.info("Controller 'createVacation' initiated.");
+        sendEmail(address, title, content);
+        log.info("Controller 'sendAnEmail' initiated.");
         return null;
+    }
+
+    void sendEmail(String to, String subject, String content) {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(to);
+
+        msg.setSubject(subject);
+        msg.setText(content);
+
+        javaMailSender.send(msg);
+
     }
 }
