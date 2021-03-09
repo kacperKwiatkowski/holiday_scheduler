95ba241252d0f84bf05b2c6c4ecf36bf25a2ed42
Kacper Kwiatkowski <kacper18k@gmail.com>
2021-03-09 17:58:32 +0100

    Registration form set up.

--- frontend/src/componenets/login.js
+++ frontend/src/componenets/login.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import { Form, Field } from 'react-final-form'
 import { useHistory } from "react-router-dom";
 import  { Redirect } from 'react-router-dom'
 import Axios from "axios";
@@ -8,6 +9,7 @@ const Login = () => {
 
     const history = useHistory();
     const [credentials, setCredentials] = useState({username: '', password: ''})
+    const [secondForm, setSecondForm] = useState(false)
 
     const handleChange = (event) => {
         const value = event.target.value;
@@ -18,13 +20,16 @@ const Login = () => {
         console.log(credentials)
     };
     
-    const handleSubmit = async (event) => {
+    const handleLoginSubmit = async (event) => {
 
     event.preventDefault()
         
      await authorize();
 
     }
+
+    const handleRegistrationSubmit = values => {
+      }
 
     const authorize =  () => {
          Axios.post("http://localhost:8080/login", 
@@ -46,24 +51,91 @@ const Login = () => {
         })
     }
 
+    const loginForm = () => {
+        return(
+            <form className={secondForm ? "loginFormWrapper loginFormShiftedWrapper" : "loginFormWrapper"} onSubmit={event => handleLoginSubmit(event)}>
+                <div className="loginFormHeader">
+                    LOG IN
+                </div>
+                <div className="loginFormContent">
+                    <label className="loginFormLabels">EMAIL</label>
+                    <input className="loginFormInput" type="text" name="username" placeholder="" value={credentials.username} onChange={event => handleChange(event)}/>
+                    <label className="loginFormLabels"> PASSWORD</label>
+                    <input className="loginFormInput" type="password" name="password" placeholder="" value={credentials.password} onChange={event => handleChange(event)}/>
+                </div>
+                <div className="loginButtonsWrapper">
+                    <button className="forgottenPasswordFormButton" type="submit" value="Submit">FORGOTTEN PASSWORD?</button>
+                    
+                    <button className="signInFormButton" type="submit" value="Submit">SIGN IN</button>
+                    <button className="signInFormButton" type="button" onClick={() => {setSecondForm(!secondForm)}}>SIGN UP</button>
+                </div>
+            </form>
+        )
+    }
+
+    const registerForm = () => {
+        return(
+            <Form
+            onSubmit={handleRegistrationSubmit}
+            render={({ onSubmit, form, values }) => (
+              <form 
+                onSubmit={onSubmit}
+                className={secondForm ? "registrationFormWrapper registrationFormShiftedWrapper" : "registrationFormWrapper"}
+              >
+                <div className="loginFormHeader">
+                    REGISTER
+                </div>
+                <div className="loginFormContent">
+                    <label className="loginFormLabels">FIRST NAME:</label>
+                    <Field
+                      className="loginFormInput" 
+                      name="firstName"
+                      component="input"
+                      type="text"
+                    />
+                    <label className="loginFormLabels">LAST NAME:</label>
+                    <Field
+                      className="loginFormInput" 
+                      name="lastName"
+                      component="input"
+                      type="text"
+                    />
+                    <label className="loginFormLabels">EMAIL:</label>
+                    <Field
+                      className="loginFormInput" 
+                      name="email"
+                      component="input"
+                      type="email"
+                    />
+                </div>
+                <div className="loginButtonsWrapper">
+                    <button
+                        className="signInFormButton"
+                        type="button"
+                        onClick={form.reset}
+                    >
+                        RESET
+                    </button>
+                    <button 
+                        type="submit"
+                        className={`signInFormButton`}
+                    >
+                        SEND
+                    </button>
+                </div>
+              </form>
+            )}
+          />
+        )
+    }
+
     if(localStorage.getItem("authorization")!=null){
         return <Redirect go to='/home'  />
     } else {
         return(
             <div className="loginSiteWrapper">
-                <form className="loginFormWrapper" onSubmit={event => handleSubmit(event)}>
-                    <label className="loginFormLabels">EMAIL</label>
-                    <input className="loginFormInput" type="text" name="username" placeholder="" value={credentials.username} onChange={event => handleChange(event)}/>
-                    <label className="loginFormLabels"> PASSWORD</label>
-                    <input className="loginFormInput" type="password" name="password" placeholder="" value={credentials.password} onChange={event => handleChange(event)}/>
-                    
-                    <div className="loginButtonsWrapper">
-                    <button className="forgottenPasswordFormButton" type="submit" value="Submit">FORGOTTEN PASSWORD?</button>
-                    
-                    <button className="signInFormButton" type="submit" value="Submit">SIGN IN</button>
-                    <button className="signInFormButton" type="submit" value="Submit">SIGN UP</button>
-                    </div>
-                </form>
+                {loginForm()}
+                {registerForm()}
             </div>
         )
     }
