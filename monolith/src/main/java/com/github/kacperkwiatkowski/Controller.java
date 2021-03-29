baeb8010217fb8b95e572b0b12a48d5f5f3aa868
Kacper Kwiatkowski <kacper18k@gmail.com>
2021-03-29 16:43:28 +0200

    Microservices update.

---
+++ monolith/src/main/java/com/github/kacperkwiatkowski/Controller.java
@@ -0,0 +1,21 @@
+package com.github.kacperkwiatkowski;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+        @RequestMapping("/")
+        public String index() {
+            return "Greetings from Spring Boot!";
+        }
+
+
+    @RequestMapping("/test")
+    public String test() {
+        return "TEST";
+    }
+
+}
---
+++ src/test/java/com/github/kacperkwiatkowski/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kacperkwiatkowski.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
